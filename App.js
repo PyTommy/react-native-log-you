8128a5ee768f9cfdfe993149ac8ab5b6f9c08db5
Your Name <you@example.com>
2020-02-16 19:55:14 +0900

    Custom font loaded

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
+    });
+  };
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Normal Text</Text>
+      <Text style={{ fontFamily: 'roboto' }}>Roboto</Text>
+      <Text style={{ fontFamily: 'roboto-bold' }}>Roboto Bold</Text>
     </View>
   );
 }
