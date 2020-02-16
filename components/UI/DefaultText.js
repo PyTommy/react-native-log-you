93f66141f42ecd56966dcf13db88691668d3d8e2
Your Name <you@example.com>
2020-02-16 20:45:34 +0900

    Colors and text components are created

---
+++ components/UI/DefaultText.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+import Colors from '../../constants/Colors';
+
+const DefaultText = props => {
+    return (
+        <Text
+            style={{ ...styles.text, ...props.style }}
+        >
+            {props.children}
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontFamily: 'roboto',
+        color: Colors.l1,
+    }
+})
+
+
+export default DefaultText
