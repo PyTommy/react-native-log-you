89775b09487f2f263b713e593d9a9a09ef94f9b2
Your Name <you@example.com>
2020-02-20 22:21:31 +0900

    Set up SQLite

---
+++ db/db.js
@@ -0,0 +1,52 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('logs.db');
+
+export const init = () => {
+    return new Promise((resolve, reject) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                `CREATE TABLE IF NOT EXISTS logs (
+                    id INTEGER PRIMARY KEY NOT NULL, 
+                    title TEXT NOT NULL, 
+                    date TEXT NOT NULL,
+                    startAt TEXT NOT NULL,
+                    stopAt TEXT NOT NULL,
+                    elapsedTime INT NOT NULL 
+                )`,
+                [],
+                () => {
+                    resolve();
+                },
+                (_, err) => {
+                    reject(err);
+                }
+            );
+        });
+    })
+};
+
+
+export const insertLog = (title, date, startAt, stopAt, elapsedTime) => {
+    return new Promise((resolve, reject) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                `INSERT INTO logs(
+                    title,
+                    date,
+                    startAt, 
+                    stopAt,
+                    elapsedTime
+                ) VALUES (?, ?, ?, ?, ?);
+                `,
+                [title, date, startAt, stopAt, elapsedTime],
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                    reject(err);
+                }
+            );
+        });
+    })
+}
