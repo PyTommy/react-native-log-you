c70f6fa081b99616a5d50e63e5824b6030e25fa4
Your Name <you@example.com>
2020-02-24 17:49:21 +0900

    Deleted some code which is not needed

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { FlatList, View, ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { dateGenerator } from '../utils/dateGenerator';
 import { fetchSummariesWithLimit } from '../store/actions/index';
 import CategoryCard from './CategoryCard';
 import NotFound from './UI/NotFound';
@@ -14,17 +13,13 @@ const ItemSummaryList = props => {
     const summaries = useSelector(state => state.summaries);
     const isoDates = Object.keys(summaries).sort((a, b) => b - a); // newer => older
 
-    const isoToday = dateGenerator(new Date()).toISOString();
-
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
     const nextFetchedISODate = isoDates[isoDates.length - 1];
 
     const loadMore = async () => {
-        console.log('loadMore start');
         if (!hasMore || loading) return;
-        console.log('loadMore continued');
 
         try {
             const limit = 15;
